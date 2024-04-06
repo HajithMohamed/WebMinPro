3da6cd4ba900a43d7a9ee540a73ac098dc11928a
Dineshs737 <dinesh77saarck@gmail.com>
2024-04-07 02:20:21 +0530

    added login page

---
+++ Web page design/official/stock.js
@@ -0,0 +1,12 @@
+function size(){
+    var a=document.getElementById("select");
+    var value=a.value;
+    var text=a.options[a.selectedIndex].text;
+    var Result;
+
+    switch(text){
+        case 1:
+        r
+            
+    }
+}
