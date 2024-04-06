65a0952b6e375a774bf0823244a067d034e615f8
HajithMohamed <165193756+HajithMohamed@users.noreply.github.com>
2024-04-06 16:03:30 +0530

    tdy work

---
+++ Web page design/official/js/jquery-3.7.1.js
@@ -0,0 +1,12 @@
+
+  function navigateToLink() {
+    const dropdown = document.getElementById("typesDropdown");
+    const selectedValue = dropdown.value;
+    window.location.href = selectedValue;
+  }
+
+  function navigateToLink1() {
+    const dropdown = document.getElementById("typesDropdown");
+    const selectedValue = dropdown.value;
+    window.location.href = selectedValue;
+  }
