55ff3f01d62844f92e0887f77dda98a797bf81ec
Dineshs737 <dinesh77saarck@gmail.com>
2024-04-08 12:47:52 +0530

    update

---
+++ Web page design/official/js/jquery-3.7.1.js
@@ -0,0 +1,18 @@
+
+  function navigateToLink() {
+    const dropdown = document.getElementById("typesDropdown");
+    const selectedValue = dropdown.value;
+    window.location.href = selectedValue;
+  }
+
+  function navigateToLink1() {
+    const dropdown = document.getElementById("sizeDropdown");
+    const selectedValue = dropdown.value;
+    window.location.href = selectedValue;
+  }
+
+  function navigateToLink2() {
+    const dropdown = document.getElementById("colorDropdown");
+    const selectedValue = dropdown.value;
+    window.location.href = selectedValue;
+  }
