c2ef6ae2b8a57760142612fb83b88ec8f87e2128
HajithMohamed <165193756+HajithMohamed@users.noreply.github.com>
2024-04-08 21:18:18 +0530

    official

---
+++ official/js/jquery-3.7.1.js
@@ -0,0 +1,23 @@
+
+  function navigateToLink() {
+    const dropdown = document.getElementById("typesDropdown");
+    const selectedValue = dropdown.value;
+    window.location.href = selectedValue;
+  }
+
+  function navigateToLink1() {
+    const dropdown = document.getElementById("sizeDropdown");
+    const selectedValue = dropdown.value;
+    window.location.href = selectedValue;
+  }
+
+  function navigateToLink2() {
+    const dropdown = document.getElementById("colorDropdown");
+    const selectedValue = dropdown.value;
+    window.location.href = selectedValue;
+  }
+  function navigateToLink3() {
+    const dropdown = document.getElementById("brandDropdown");
+    const selectedValue = dropdown.value;
+    window.location.href = selectedValue;
+  }
